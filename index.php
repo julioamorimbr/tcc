2fe79685e5649cd7356e1054b276a6b5d6924624
System Administrator <root@MacBooks-MacBook-Pro.local>
2022-11-20 23:58:37 -0300

    Sistema de login e index

---
+++ index.php
@@ -0,0 +1,19 @@
+<head>
+<link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
+<script type="text/javascript" src="js/video.js"></script>
+<title> Index - Service Desk</title>
+</head>
+
+<section class="fullsize-video-bg">
+	<div class="inner">
+		<div>
+			<h1>Service Desk</h1>
+      <p>Atendimento automatizado em toda a triagem
+		</div>
+	</div>
+	<div id="video-viewport">
+		<video width="1920" height="1280" autoplay muted loop>
+			<source src="video.mp4" type="video/mp4" />
+		</video>
+	</div>
+</section>
